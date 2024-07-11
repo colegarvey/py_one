8e8ce83a945f7afaacbcc40b557ac5f8ec46791e
Cole Garvey <colemiester19@outlook.com>
2024-07-10 22:22:51 -0400

    file setup

--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
 from Person import * 
 import tkinter as tk
+from sys import argv,exit
 
+'''
+python main.py users.csv data.csv
+'''
 def main():
-    window = tk.Tk()
-    window.title("MAIN WINDOW")
+    if len(argv) != 3:
+        print(" MISSING FILES ARGS ".center(30,'='))
+        exit(1)
+    
+    users,data = argv[1],argv[2]
 
-    label = tk.Label(window,text='hello',padx=20,pady=20)
-    label.pack()
+    
+    
+    
+    # window = tk.Tk()
+    # window.title("MAIN WINDOW")
 
-    window.mainloop()
+    # label = tk.Label(window,text='hello',padx=20,pady=20)
+    # label.pack()
+
+    # window.mainloop()
     # return 
 
 
